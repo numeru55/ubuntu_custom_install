b4fe6cc031a236c2ec38458ee85428f0ad2ea5ed
numeru55 <89648361+numeru55@users.noreply.github.com>
2022-02-07 23:21:51 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -170,7 +170,7 @@ locale-gen --keep-existing
 
 # install my essential package
 
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 apt-get update
 apt-get install -y google-chrome-stable
